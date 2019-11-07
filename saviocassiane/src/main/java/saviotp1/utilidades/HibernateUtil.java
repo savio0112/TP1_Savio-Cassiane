ebfdbadc1f35addae3dda393aea15efdf9499fa5
sangueazul <kassianeterra@gmail.com>
2019-11-07 11:42:11 -0200

    segunda parte da aulinha

--- saviocassiane/src/main/java/saviotp1/utilidades/HibernateUtil.java
+++ saviocassiane/src/main/java/saviotp1/utilidades/HibernateUtil.java
@@ -3,30 +3,15 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package utilidades;
+package saviotp1.utilidades;
 
-import entidades.Usuario;
+import saviotp1.entidades.Usuario;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.AnnotationConfiguration;
 
 public class HibernateUtil {
 
-//    private static final SessionFactory sessionFactory;
-//    static {
-//        try {
-//            sessionFactory = new AnnotationConfiguration()
-//                    .configure().buildSessionFactory();
-//        } catch (Throwable ex) {
-//            // Log exception!
-//            throw new ExceptionInInitializerError(ex);
-//        }
-//    }
-//
-//    public static Session getSession()
-//            throws HibernateException {
-//        return sessionFactory.openSession();
-//    }
     
     private static SessionFactory factory;
 
